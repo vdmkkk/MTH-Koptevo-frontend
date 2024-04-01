a633f8f480ca12418fb4144b507c2045923eae9c
AlexxxGorin <alexgorin37@gmail.com>
2024-04-01 17:39:43 +0300

    layout changes

--- mth-2024-frontend/src/pages/onboarding-page.jsx
+++ mth-2024-frontend/src/pages/onboarding-page.jsx
@@ -1,9 +1,15 @@
 import '../styles/onboarding-page.css';
+import Layout from '../components/reusable/layout.jsx';
 
 function OnboardingPage() {
   return (
     <div className="App">
-      
+        <Layout>
+          
+
+
+
+        </Layout>
     </div>
   );
 }
