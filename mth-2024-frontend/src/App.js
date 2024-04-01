79f3d06fef66ce39e01cad9c0b347bce1b78c252
vdmkkk <soloviov.vad@gmail.com>
2024-04-01 16:58:27 +0300

    some login logic from template

--- mth-2024-frontend/src/App.js
+++ mth-2024-frontend/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom"
+import Login from './Login';
+import { useCookies } from "react-cookie";
+import { useEffect, useState } from 'react';
+
 
 function App() {
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* не забудь изменить index.js !!! */}
+      <Routes>
+        <Route path="/" element={ <Home/> } />
+        <Route path="login" element={ <Login/> } />
+        <Route path="*" element={<Navigate to="/" replace />}/> 
+        {/* это для потеряшек */}
+      </Routes>
     </div>
   );
+}
+
+
+function Home() {
+  const [cookies, setCookie] = useCookies(["JWT", "Session"]); // как я понял, указываем интересующие нас куки (?)
+
+  // useEffect is optional. Should use, when you restrict access to guest users.
+
+
+  function handleExit() {
+    document.cookie = "JWT=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "Session=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  }
+
+  return(
+    <div className='App'>
+      {cookies.JWT && <h1>Ваш JWT: {cookies.JWT.slice(0, 10)}...{cookies.JWT.slice(cookies.JWT.length - 10, cookies.JWT.length)}</h1>}
+      {cookies.JWT && <h1>Ваш Session: {cookies.Session.slice(0, 10)}...{cookies.Session.slice(cookies.Session.length - 10, cookies.Session.length)}</h1>}
+      <button onClick={() => handleExit()}>Выйти</button>
+    </div>
+  )
 }
 
 export default App;
