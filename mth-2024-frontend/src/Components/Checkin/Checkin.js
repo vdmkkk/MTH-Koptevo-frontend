89cfed89027849bc2f97a8dba0b7a8380ee694ed
vdmkkk <soloviov.vad@gmail.com>
2024-04-07 09:01:10 +0300

    checkin?

--- mth-2024-frontend/src/Components/Checkin/Checkin.js
+++ mth-2024-frontend/src/Components/Checkin/Checkin.js
@@ -2,16 +2,22 @@ import axios from "axios";
 import { useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import {useNavigate} from "react-router"
+
 
 function Checkin() {
+    const [cookies, setCookie] = useCookies(["JWT"]);
+    const navigate = useNavigate();
     const cipher  = useParams()["cipher"];
     useEffect(() => {
-        axios.post(`http://0.0.0.0:8080/checkin?cipher=${cipher}&userId=1`);
+        if (cookies.JWT)
+        axios.post(`${process.env.REACT_APP_ZAMAN_API}/user/check_in?cipher=${cipher}&user_id=${cookies.JWT}`).then(res => {
+            navigate(`/checkin/show${res.data}`)
+        })
     }, [])
     return(
         <div>
-            <Navigate replace to="/checkin/show/123123123" />
-            <h1>{cipher}</h1>
         </div>
     )
 }
